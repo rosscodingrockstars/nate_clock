83bbe018c8821298257dd5456744d7c783d4c90c
rosscodingrockstars <hey@jenniferross.tech>
2021-03-03 16:59:15 -0500

    added wake-up functions

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
-let wakeUpValue = $("#wake-up").val();
-console.log(wakeUpValue)
+let wakeUpValue = $( "#wake-up" ).val()
+
+
+$( "#wake-up" ).change(function() {
+    console.log($( "#wake-up" ).val())
+});
+//"7:00 AM to 8:00 AM"
+if (wakeUpValue === "7:00 AM to 8:00 AM") {
+$("#mainphoto")
+}
