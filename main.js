87dc7de426644ccc05a740de397e508a685cd7fa
rosscodingrockstars <hey@jenniferross.tech>
2021-03-10 16:57:36 -0500

    made wakeupvalue change to the value person picks

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 let wakeUpValue = $( "#wake-up" ).val()
-
+// wakeUpValue is test
 
 $( "#wake-up" ).change(function() {
-    console.log($( "#wake-up" ).val())
-});
+    wakeUpValue = $("#wake-up").val();
+
 //"7:00 AM to 8:00 AM"
+console.log(wakeUpValue);
 if (wakeUpValue === "7:00 AM to 8:00 AM") {
-$("#mainphoto")
+$("#mainphoto").attr("src", "images/wake-up-cat-text.png");
+
 }
+});
