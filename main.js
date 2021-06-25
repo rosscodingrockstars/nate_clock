e3b4b580621c08f0445dbd2812d6b5c9a6326baa
rosscodingrockstars <hey@jenniferross.tech>
2021-06-25 15:51:54 -0400

    finished Cat Clock and improved compatibility on all devices.

--- main.js
+++ main.js
@@ -70,13 +70,13 @@ $( "#partybutton" ).click(function() {
     let hours = time.getHours();
     let minutes = time.getMinutes();
     let seconds = time.getSeconds();
-    var ampm = hours >= 12 ? 'pm' : 'am';
+    var ampm = hours >= 12 ? 'PM' : 'AM';
     hours = hours % 12;
     hours = hours ? hours : 12; // the hour '0' should be '12'
     minutes = minutes < 10 ? '0'+minutes : minutes;
     seconds = seconds < 10 ? '0'+seconds : seconds;
 
-    const combinedTime = `${hours}:${minutes}:${seconds}:${ampm}`;
+    const combinedTime = `${hours}:${minutes}:${seconds} ${ampm}`;
     $( "#clock" ).text(combinedTime);
  }
 setInterval(showtime, 1000);
